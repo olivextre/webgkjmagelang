36fd2e607aef10c8733e0ac7027239daa2b008f0
olivextre <sb32423@gmail.com>
2024-08-18 23:21:53 +0700

    warta

--- app/Models/Warta.php
+++ app/Models/Warta.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Warta extends Model
 {
     use HasFactory;
-    protected $fillable = ['name','file'];
+    protected $fillable = ['name','file','tanggal'];
 }
